29a7c85b0218acb8b0707bdc4f77a46a636d79bd
s0yz <41223213+s0yz@users.noreply.github.com>
2018-11-29 14:32:31 -0500

    Update Commande.cs

--- Concept/Concept/Commande.cs
+++ Concept/Concept/Commande.cs
@@ -2,18 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace Concept
 {
-    public class Commande : IEnumerable<Produit>
-    {
-        //private int m_Identifiant;
-        //private Utilisateur m_Client;
-        //private string m_AdresseLivraison;
-        //private DateTime m_Date;
-        //private StatutCommande m_Statut;
-        private List<Produit> m_ListeProduits;
+    public class Commande : IEnumerable<KeyValuePair<Produit, uint>>
+    {        
+        private Dictionary<Produit, uint> m_Produits = new Dictionary<Produit, uint>();
 
         public Commande(Utilisateur p_client) :
             this(p_client, BDGestion.Instance.GetStatutCommande(0), p_client.getAdresse())
@@ -31,60 +25,42 @@ namespace Concept
             this.AdresseLivraison = p_adresse;
             this.Date = DateTime.Now;
             this.Statut = p_statut;
-            this.m_ListeProduits = new List<Produit>();
         }
 
         public int Identifiant { get; private set; }
 
-        //public Utilisateur getClient()
-        //{
-        //    return this.m_Client;
-        //}
         public Utilisateur Client { get; private set; }
 
-        //public string getAdresse()
-        //{
-        //    return this.m_AdresseLivraison;
-        //}
-        //public void setAdresse(string p_adresse)
-        //{
-        //    this.m_AdresseLivraison = p_adresse;
-        //}
         public string AdresseLivraison { get; set; }
         
         public DateTime Date { get; private set; }
 
         public StatutCommande Statut { get; set; }
 
-        public void Ajouter(Produit p_produit, int p_quantite)
+        public void Ajouter(Produit p_produit, uint p_quantite)
         {
-            for (int i = 0; i < p_quantite; i++)
+            if (this.m_Produits.ContainsKey(p_produit))
             {
-                this.m_ListeProduits.Add(p_produit);
+                this.m_Produits[p_produit] += p_quantite;
             }
+            else this.m_Produits.Add(p_produit, p_quantite);
         }
 
         public void Retirer(Produit p_produit, uint p_quantite)
         {
-            for (int i = 0; i < p_quantite; i++)
+            if (this.m_Produits.ContainsKey(p_produit))
             {
-                this.m_ListeProduits.Remove(p_produit);
+                if ((this.m_Produits[p_produit] -= p_quantite) < 1)
+                {
+                    this.m_Produits.Remove(p_produit);
+                }
             }
         }
+        
+        public double CalculerTotal() => this.Sum(p => p.Key.getPrix() * p.Value);
 
-        //public double CalculerTotal()
-        //{
-        //    double prix = 0;
-        //    foreach (Produit item in this.m_ListeProduits)
-        //    {
-        //        prix += item.getPrix();
-        //    }
-        //    return prix;
-        //}
-        public double CalculerTotal() => this.Sum(p => p.getPrix());
+        public IEnumerator<KeyValuePair<Produit, uint>> GetEnumerator() => this.m_Produits.GetEnumerator();
 
-        public IEnumerator<Produit> GetEnumerator() => m_ListeProduits.GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => m_ListeProduits.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => this.m_Produits.GetEnumerator();
     }
 }
