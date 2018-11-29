7272837caf4cf82a1659f6c7008ad27f6673fede
s0yz <41223213+s0yz@users.noreply.github.com>
2018-11-29 14:59:40 -0500

    Update Commande.cs

--- Concept/Concept/Commande.cs
+++ Concept/Concept/Commande.cs
@@ -5,16 +5,9 @@ using System.Linq;
 
 namespace Concept
 {
-<<<<<<< HEAD
-    public class Commande : IEnumerable<Produit>
-    {
-        
-        private List<Produit> m_ListeProduits;
-=======
     public class Commande : IEnumerable<KeyValuePair<Produit, uint>>
     {        
         private Dictionary<Produit, uint> m_Produits = new Dictionary<Produit, uint>();
->>>>>>> master
 
         public Commande(Utilisateur p_client) :
             this(p_client, BDGestion.Instance.GetStatutCommande(0), p_client.getAdresse())
@@ -35,16 +28,9 @@ namespace Concept
         }
 
         public int Identifiant { get; private set; }
-
-<<<<<<< HEAD
-      
+        
         public Utilisateur Client { get; private set; }
 
-      
-=======
-        public Utilisateur Client { get; private set; }
-
->>>>>>> master
         public string AdresseLivraison { get; set; }
         
         public DateTime Date { get; private set; }
@@ -73,14 +59,7 @@ namespace Concept
         
         public double CalculerTotal() => this.Sum(p => p.Key.getPrix() * p.Value);
 
-<<<<<<< HEAD
-        
-        public double CalculerTotal() => this.Sum(p => p.getPrix());
-
-        public IEnumerator<Produit> GetEnumerator() => m_ListeProduits.GetEnumerator();
-=======
         public IEnumerator<KeyValuePair<Produit, uint>> GetEnumerator() => this.m_Produits.GetEnumerator();
->>>>>>> master
 
         IEnumerator IEnumerable.GetEnumerator() => this.m_Produits.GetEnumerator();
     }
