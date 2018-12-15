b0646410506194c1fc6acfd7ff2468d56977345d
BobAlcatraz <37194652+BobAlcatraz@users.noreply.github.com>
2018-12-15 16:55:35 -0500

    Update de weekend 1

--- Concept/Concept/BDgestion.cs
+++ Concept/Concept/BDgestion.cs
@@ -9,30 +9,40 @@ namespace Concept
 {
     public class BDGestion
     {
-        public readonly StatutCommande[] STATUT_COMMANDE;
-        public readonly CategorieProduit[] CATEGORIE_PRODUIT;
-        public readonly TypeUtilisateur[] TYPE_UTILISATEUR;
+        public readonly Dictionary<char, StatutCommande> STATUT_COMMANDE;
+        public readonly Dictionary<char, CategorieProduit> CATEGORIE_PRODUIT;
+        public readonly Dictionary<char, TypeUtilisateur> TYPE_UTILISATEUR;
         private SqlConnection m_Connection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\admin\Documents\GitHub\CS_TP04\Concept\Concept\App_Data\Concept.mdf;Integrated Security = True");
 
         private BDGestion() {
             m_Connection.Open();
-            this.STATUT_COMMANDE = this.GetStatutsCommande().ToArray();
-            this.CATEGORIE_PRODUIT = this.GetCategoriesProduit().ToArray();
-            this.TYPE_UTILISATEUR = this.GetTypesUtilisateur().ToArray();
+            foreach (StatutCommande statut in this.GetStatutsCommande())
+            {
+                this.STATUT_COMMANDE.Add(statut.ID, statut);
+            }
+            foreach (CategorieProduit categorie in this.GetCategoriesProduit())
+            {
+                this.CATEGORIE_PRODUIT.Add(categorie.Id, categorie);
+            }
+            foreach (TypeUtilisateur type in this.GetTypesUtilisateur())
+            {
+                this.TYPE_UTILISATEUR.Add(type.Id, type);
+            }
         }
 
         public static BDGestion Instance { get; } = new BDGestion();
 
         public void ajouter(Commande p_Commande, int p_Id_User, int p_Id_Resto)
         {
-            int id_Commande = 0;
             SqlCommand command = new SqlCommand("CreerCommande", this.m_Connection)
             { CommandType = CommandType.StoredProcedure };
             command.Parameters.AddWithValue("@p_Id_Utilisateur", p_Id_User);
             command.Parameters.AddWithValue("@p_Id_Restaurant", p_Id_Resto);
-            command.Parameters.AddWithValue("@p_Id_Commande", id_Commande);
             command.Parameters.AddWithValue("@p_Adresse", p_Commande.AdresseLivraison);
-            command.ExecuteNonQuery();
+            SqlDataReader receiver = command.ExecuteReader();
+            //Verify
+            receiver.Read();
+            int id_Commande = (int)receiver["id_commande"];
             command = new SqlCommand("AjouterDansCommande", this.m_Connection){ CommandType = CommandType.StoredProcedure };
             foreach (KeyValuePair<Produit, uint> produit in p_Commande)
             {
@@ -57,7 +67,12 @@ namespace Concept
 
         public void ajouter(Restaurant p_Restaurant)
         {
-            // TODO
+            SqlCommand command = new SqlCommand("CreerRestaurant", this.m_Connection)
+            { CommandType = CommandType.StoredProcedure };
+            command.Parameters.AddWithValue("@p_Adresse", p_Restaurant.Adresse);
+            command.Parameters.AddWithValue("@p_Telephone", p_Restaurant.Telephone);
+            command.Parameters.AddWithValue("@p_Path", p_Restaurant.ImagePath);
+            command.ExecuteNonQuery();
         }
 
         public void ajouter(Utilisateur p_Utilisateur)
@@ -73,6 +88,28 @@ namespace Concept
             command.ExecuteNonQuery();
         }
 
+        public void ajouter(Menu p_Menu, int p_Id_Resto)
+        {
+            SqlCommand command = new SqlCommand("CreerMenu", this.m_Connection)
+            { CommandType = CommandType.StoredProcedure };
+            command.Parameters.AddWithValue("@p_Restaurant", p_Id_Resto);
+            command.ExecuteNonQuery();
+            command = new SqlCommand("GetMenuResto", this.m_Connection)
+            { CommandType = CommandType.StoredProcedure };
+            SqlDataReader reader = command.ExecuteReader();
+            //Verifier
+            reader.Read();
+            int id_menu = (int)reader["id_menu"];
+            foreach (Produit produit in p_Menu.ListeProduit)
+            {
+                command = new SqlCommand("AjouterAuMenu", this.m_Connection)
+                { CommandType = CommandType.StoredProcedure };
+                command.Parameters.AddWithValue("@p_Id_Menu", id_menu);
+                command.Parameters.AddWithValue("@p_Id_Menu", produit.Id);
+                command.ExecuteNonQuery();
+            }
+        }
+
         // Peut-être pas nécessaire...
         public void Sauvegarder()
         {
@@ -85,10 +122,28 @@ namespace Concept
             return null;
         }
 
-        public IList<Commande> GetCommandes(int p_Id)
+        public IList<Commande> GetCommandes(int p_Id_User)
         {
-            // TODO
-            return null;
+            SqlCommand command = new SqlCommand("GetCommandesParUser", this.m_Connection)
+            { CommandType = CommandType.StoredProcedure };
+            command.Parameters.AddWithValue("@p_Id_Utilisateur", p_Id_User);
+            SqlDataReader reader = command.ExecuteReader();
+            List<Commande> commandes = new List<Commande>();
+            while (reader.Read())
+            {
+                commandes.Add(new Commande(
+                    (int)reader["id_commande"],
+                    this.GetUtilisateur((int)reader["id_utilisateur"]),
+                    STATUT_COMMANDE[(char)reader["id_statut"]],
+                    (string)reader["adresse"],
+                    (DateTime)reader["date_livraison"]));
+                IDictionary<Produit, uint> produits = this.GetProduits((int)reader["id_commande"]);
+                foreach (KeyValuePair<Produit, uint> produit in produits)
+                {
+                    commandes.Last<Commande>().Ajouter(produit.Key, produit.Value);
+                }
+            }
+            return commandes;
         }
 
         public IList<Commande> GetCommandesParStatut(int p_Id_Resto, char p_Id_Statut)
@@ -97,14 +152,61 @@ namespace Concept
             { CommandType = CommandType.StoredProcedure };
             command.Parameters.AddWithValue("@p_Id_Resto", p_Id_Resto);
             command.Parameters.AddWithValue("@p_Id_Statut", p_Id_Statut);
-            command.ExecuteNonQuery();
-            return null;
+            SqlDataReader reader = command.ExecuteReader();
+            List<Commande> commandes = new List<Commande>();
+            while (reader.Read()) {
+                commandes.Add(new Commande(
+                    (int)reader["id_commande"], 
+                    this.GetUtilisateur((int)reader["id_utilisateur"]), 
+                    STATUT_COMMANDE[(char)reader["id_statut"]], 
+                    (string)reader["adresse"], 
+                    (DateTime)reader["date_livraison"]));
+                IDictionary<Produit, uint> produits = this.GetProduits((int)reader["id_commande"]);
+                foreach (KeyValuePair<Produit, uint> produit in produits)
+                {
+                    commandes.Last<Commande>().Ajouter(produit.Key, produit.Value);
+                }
+            }
+            return commandes;
         }
 
         public IList<Produit> GetProduits()
         {
-            // TODO
-            return null;
+            SqlCommand command = new SqlCommand("GetProduits", this.m_Connection)
+            { CommandType = CommandType.StoredProcedure };
+            SqlDataReader reader = command.ExecuteReader();
+            List<Produit> produits = new List<Produit>();
+            while (reader.Read()) {
+                produits.Add(new Produit(
+                    (int)reader["id_produit"],
+                    (string)reader["nom"],
+                    (string)reader["desc_prod"],
+                    (decimal)reader["prix"],
+                    (string)reader["path_image"],
+                    this.CATEGORIE_PRODUIT[(char)reader["id_cat"]]
+                    ));
+            }
+            return produits;
+        }
+
+        public IDictionary<Produit, uint> GetProduits(int p_Id_Commande)
+        {
+            SqlCommand command = new SqlCommand("GetProduitsDeCommande", this.m_Connection) { CommandType = CommandType.StoredProcedure };
+            SqlDataReader product_reader = command.ExecuteReader();
+            Dictionary<Produit, uint> commandes = new Dictionary<Produit, uint>();
+            while (product_reader.Read())
+            {
+                commandes.Add(new Produit(
+                        (int)product_reader["id_produit"],
+                        (string)product_reader["nom"],
+                        (string)product_reader["desc_prod"],
+                        (decimal)product_reader["prix"],
+                        (string)product_reader["path_image"],
+                        this.CATEGORIE_PRODUIT[(char)product_reader["id_cat"]]
+                    ), 
+                    (uint)product_reader["nb"]);
+            }
+            return commandes;
         }
 
         public Produit GetProduit(int p_Id)
@@ -115,8 +217,47 @@ namespace Concept
 
         public IList<Restaurant> GetRestaurants()
         {
-            // TODO
-            return null;
+            SqlCommand command = new SqlCommand("GetRestaurant", this.m_Connection) { CommandType = CommandType.StoredProcedure };
+            SqlDataReader reader = command.ExecuteReader();
+            List<Restaurant> restaurants = new List<Restaurant>();
+            SqlDataReader menuGetter;
+            while (reader.Read())
+            {
+                restaurants.Add(new Restaurant(
+                    (int)reader["id_restaurant"],
+                    (string)reader["adresse"],
+                    (string)reader["telephone"],
+                    (string)reader["path_image"]
+                    ));
+                IList<Produit> produits = new List<Produit>();
+                command = new SqlCommand("GetMenuResto", this.m_Connection) { CommandType = CommandType.StoredProcedure };
+                command.Parameters.AddWithValue("@p_Id_Resto", reader["id_restaurant"]);
+                menuGetter = command.ExecuteReader();
+                //Verifier
+                menuGetter.Read();
+                restaurants.Last<Restaurant>().Menu = GetMenu((int)menuGetter["id_menu"]);
+            }
+            return restaurants;
+        }
+
+        public Menu GetMenu(int p_Id)
+        {
+            Menu menu = new Menu();
+            SqlCommand command = new SqlCommand("GetMenu", this.m_Connection) { CommandType = CommandType.StoredProcedure };
+            command.Parameters.AddWithValue("@p_Id_Menu", p_Id);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                menu.Ajouter(new Produit(
+                    (int)reader["id_produit"],
+                    (string)reader["nom"],
+                    (string)reader["desc_prod"],
+                    (decimal)reader["prix"],
+                    (string)reader["path_image"],
+                    CATEGORIE_PRODUIT[(char)reader["id_cat"]]
+                    ));
+            }
+            return menu;
         }
 
         public Restaurant GetRestaurant(int p_Id)
--- Concept/Concept/Utilisateur.cs
+++ Concept/Concept/Utilisateur.cs
@@ -18,6 +18,22 @@ namespace Concept
             this.Email = p_email;
             this.Restaurant = p_restaurant;
         }
+
+        public Utilisateur(int p_id, string p_nom, string p_passe, TypeUtilisateur p_typeU,
+            string p_adresse, string p_email, Restaurant p_restaurant)
+        {
+            this.Id = p_id;
+            this.Nom = p_nom;
+            this.MotDePasse = p_passe;
+            this.Type = p_typeU;
+            this.Commandes = new List<Commande>();
+            this.Adresse = p_adresse;
+            this.Email = p_email;
+            this.Restaurant = p_restaurant;
+        }
+
+        public int Id { get; private set; }
+
         public string Nom { get; private set; }
 
         public string MotDePasse { get; private set; }
@@ -32,9 +48,15 @@ namespace Concept
 
         public Restaurant Restaurant { get; private set; }
 
-        public void AjouterCommande(Commande p_commande)
+        public void Commit()
+        {
+            BDGestion.Instance.ajouter(this);
+        }
+
+        public void AjouterCommande(Commande p_commande, int p_Id_Resto)
         {
             this.Commandes.Add(p_commande);
+            BDGestion.Instance.ajouter(p_commande, this.Id, p_Id_Resto);
         }
     }
 }
